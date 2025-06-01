fdca4602bba6e15460a0ac1029715cfdf2f6c70f
kostya-savchenko <kostyantyn.savchenko@gmail.com>
2025-06-01 22:44:04 +0300

    added solution 2nd-HW OnlineBookStoreApplication v1.1(remove comments)

--- src/main/java/kristar/projects/OnlineBookStoreApplication.java
+++ src/main/java/kristar/projects/OnlineBookStoreApplication.java
@@ -18,24 +18,4 @@ public class OnlineBookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
-    //    @Bean
-    //    public CommandLineRunner commandLineRunner(BookService bookService) {
-    //        return args -> {
-    //            BookDto book1 = new BookDto();
-    //            book1.setTitle("One Hundred Years of Solitude");
-    //            book1.setAuthor("Gabriel Garcia Marquez");
-    //            book1.setIsbn("san-123-45-02t");
-    //            book1.setPrice(BigDecimal.valueOf(499));
-    //            bookService.save(book1);
-    //
-    //            Book book2 = new Book();
-    //            book2.setTitle("Arch of Triumph");
-    //            book2.setAuthor("Erich Maria Remarque");
-    //            book2.setIsbn("san-126-85-02t");
-    //            book2.setPrice(BigDecimal.valueOf(900));
-    //            bookService.save(book2);
-    //
-    //            System.out.println(bookService.getAll());
-    //        };
-    //    }
 }
