2c4434c2b7e24d29c8869c3e34746a8ef1e8fdd1
kostya-savchenko <kostyantyn.savchenko@gmail.com>
2025-06-19 22:33:43 +0300

    added solution 8th-HW OnlineBookStoreApplication v1.1 (added swagger documentation for registration)

--- src/main/java/kristar/projects/controller/AuthenticationController.java
+++ src/main/java/kristar/projects/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package kristar.projects.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kristar.projects.dto.userdto.UserRegistrationRequestDto;
 import kristar.projects.dto.userdto.UserResponseDto;
 import kristar.projects.exception.RegistrationException;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication", description = "Authentication and Registration APIs")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
 
+    @Operation(summary = "User registration", description = "Register a new user account")
     @PostMapping("/registration")
     public UserResponseDto registerUser(@RequestBody UserRegistrationRequestDto requestDto)
             throws RegistrationException {
