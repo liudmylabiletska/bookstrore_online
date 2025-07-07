f4a43ae0673ad69ad05fa4d53d471f8cf84cbf35
kostya-savchenko <kostyantyn.savchenko@gmail.com>
2025-07-07 15:43:47 +0300

    added solution 13th-HW OnlineBookStoreApplication v1.0 (added Docker)

--- src/main/java/kristar/projects/OnlineBookStoreApplication.java
+++ src/main/java/kristar/projects/OnlineBookStoreApplication.java
@@ -2,9 +2,13 @@ package kristar.projects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "kristar.projects.repository.user")
+@EntityScan(basePackages = "kristar.projects.model")
 @EnableSpringDataWebSupport(pageSerializationMode
         = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OnlineBookStoreApplication {
