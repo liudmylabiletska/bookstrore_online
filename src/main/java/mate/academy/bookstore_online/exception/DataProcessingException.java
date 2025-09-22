0c9e1546f5903a2f6dd52f6bd91953cf807a6678
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-09-22 18:00:43 +0200

    Create added .gitignore, application.properties and test/application.properties, Book.java, BookRepository.java, BookRepositoryImpl.java, BookService.java and BookServiceImpl.java, BookstoreOnlineApplication.java checkstyle.xml, DataProccessingException.java and pom.xml and Unversioned Files modific application.properties and test/application.properties, book.class /target/classes, Book.java

---
+++ src/main/java/mate/academy/bookstore_online/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.bookstore_online.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
