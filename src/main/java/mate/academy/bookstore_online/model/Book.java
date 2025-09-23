fcdb5328311b0607071de459ca403e8794c67984
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-09-23 20:22:13 +0200

    create .gitignore, modified Book.java, Book.class, boostore_online-0.0.1-SNAPSHOT.jar, boostore_online-0.0.1-SNAPSHOT.jar.original, BookstoreOnlineApplication.class, checkstyle-cachefile, checkstyle-result.xml

--- src/main/java/mate/academy/bookstore_online/model/Book.java
+++ src/main/java/mate/academy/bookstore_online/model/Book.java
@@ -6,27 +6,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
-import lombok.Data;
-import lombok.experimental.Accessors;
 
 @Entity
-@Data
-@Accessors(chain = true)
+@Getter
+@Setter
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(nullable = false, unique = true)
     private String isbn;
+
     @Column(nullable = false)
     private BigDecimal price;
     private String description;
-    @Column(name = "cover_image")
+
     private String coverImage;
 }
