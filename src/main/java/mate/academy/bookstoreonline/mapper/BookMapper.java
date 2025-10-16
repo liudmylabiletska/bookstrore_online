4d66c35dc4dcbcc8b11ea8350d0434b329b31fd0
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-16 23:20:53 +0200

    FIX: Resolve MapStruct 'unmapped id' warning in BookMapper

--- src/main/java/mate/academy/bookstoreonline/mapper/BookMapper.java
+++ src/main/java/mate/academy/bookstoreonline/mapper/BookMapper.java
@@ -5,10 +5,12 @@ import mate.academy.bookstoreonline.dto.BookDto;
 import mate.academy.bookstoreonline.dto.CreateBookRequestDto;
 import mate.academy.bookstoreonline.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
+    @Mapping(target = "id", ignore = true)
     Book toModel(CreateBookRequestDto createBookRequestDto);
 }
