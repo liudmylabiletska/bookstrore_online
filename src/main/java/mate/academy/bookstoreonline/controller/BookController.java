3f0b04fdc93686111214fe124cd1750efc511cce
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-28 22:29:21 +0100

    Added BookController.java, CreateBookRequestDto.java, CustomGlobalExceptionHandler.java, modified pom.xml

--- src/main/java/mate/academy/bookstoreonline/controller/BookController.java
+++ src/main/java/mate/academy/bookstoreonline/controller/BookController.java
@@ -1,12 +1,13 @@
-package mate.academy.bookonline.controller;
+package mate.academy.bookstoreonline.controller;
 
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;
 import mate.academy.bookstoreonline.dto.BookDto;
+import mate.academy.bookstoreonline.dto.BookSearchParametersDto;
 import mate.academy.bookstoreonline.dto.CreateBookRequestDto;
 import mate.academy.bookstoreonline.dto.UpdateBookRequestDto;
 import mate.academy.bookstoreonline.service.BookService;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,30 +24,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
+
     @GetMapping
-    public List<BookDto> getAll() {
+    public List<BookDto> findAll() {
         return bookService.findAll();
     }
+
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
-        return bookService.save(requestDto);
+    public BookDto save(@RequestBody @Valid CreateBookRequestDto requestBook) {
+        return bookService.save(requestBook);
     }
 
     @PutMapping("/{id}")
     public BookDto updateBook(
             @PathVariable Long id,
-            @RequestBody UpdateBookRequestDto requestDto) {
-        return bookService.update(id, requestDto);
+            @RequestBody @Valid UpdateBookRequestDto requestBook) {
+        return bookService.update(id, requestBook);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteBookById(@PathVariable Long id) {
         bookService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(@Valid BookSearchParametersDto searchParameters) {
+        return bookService.search(searchParameters);
     }
 }
--- src/main/java/mate/academy/bookstoreonline/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstoreonline/dto/CreateBookRequestDto.java
@@ -1,15 +1,22 @@
 package mate.academy.bookstoreonline.dto;
 
-
-import java.math.BigDecimal;
 import lombok.Data;
+import java.math.BigDecimal;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
-    private String isbn;
+    @NotNull
+    @Positive
     private BigDecimal price;
     private String description;
+    @NotBlank
+    private String isbn;
     private String coverImage;
 }
