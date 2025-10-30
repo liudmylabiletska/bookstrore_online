c668dea5ad972f49986bbcc4a876ccb6f49a23fc
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-10-30 22:02:38 +0100

    modified BookController.java, CreateBookRequestDto.java, CustomGlobalExceptionHandler.java, modified BookController

--- src/main/java/mate/academy/bookstoreonline/controller/BookController.java
+++ src/main/java/mate/academy/bookstoreonline/controller/BookController.java
@@ -1,16 +1,24 @@
 package mate.academy.bookstoreonline.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstoreonline.dto.BookDto;
 import mate.academy.bookstoreonline.dto.BookSearchParametersDto;
 import mate.academy.bookstoreonline.dto.CreateBookRequestDto;
 import mate.academy.bookstoreonline.dto.UpdateBookRequestDto;
 import mate.academy.bookstoreonline.service.BookService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
+import jakarta.validation.Valid;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
