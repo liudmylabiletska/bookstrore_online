9e03743e99bb723455a2e23b669cbe3057ee5890
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-11-30 00:47:23 +0100

    modified application.properties/test, pom.xml, db.changelog.master-yaml, added 02-add-new-column, 03-create-users-table.yaml, AuthController.java, FileMatch.java, FileMatchValidation.java, RegistrationException.java, User.java, UserMapper.java, UserRegistrationRequestDto.java, UserRepositiry.java, UserResponseDto.java, UserService.java, UserServiceimpl.java modified User.java

--- src/main/java/mate/academy/bookstoreonline/model/User.java
+++ src/main/java/mate/academy/bookstoreonline/model/User.java
@@ -1,14 +1,13 @@
 package mate.academy.bookstoreonline.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "users")
